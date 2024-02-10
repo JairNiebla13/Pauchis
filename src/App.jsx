78e2a805e608f42c5a01cef22a8f7ceacb897799
JairNiebla13 <ibautistag1600@alumno.ipn.mx>
2024-02-09 23:39:31 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Yes from "./pages/Yes";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Yes" element={<Yes />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
